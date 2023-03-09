50c552b5b3e4263462c6cccf368569541093521e
aaaaarc <re.lumence@gmail.com>
2023-03-09 20:04:31 +0300

    initial_commit

---
+++ src/main/java/ru/mirea/vorontsovaov/lesson1/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.mirea.vorontsovaov.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.aaaaa);
+    }
+}
